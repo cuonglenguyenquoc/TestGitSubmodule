f2910b5a34ff8ed0262ff527af49146063e0af3d
cuonglnq <cuong.le@chotot.vn>
2022-10-31 11:13:05 +0700

    initial project

---
+++ TestGitSubmodule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGitSubmodule
+//
+//  Created by Cuong Le on 31/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
